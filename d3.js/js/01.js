5d4c9c266d342432c3ceebc2f6c1ff5c76615a88
박준호 <junho@brunt.co>
2016-12-10 15:03:13 +0900

    d3 and pure js code comparison

--- d3.js/js/01.js
+++ d3.js/js/01.js
@@ -18,5 +18,21 @@ window.onload = function(){
     // d3.으로 시작하는 것은 select()로 시작되는 그룹을 만드는 것 wrapper method라고 생각하면 됨.
     
 // select
- 
+
+    // JS의 selector
+    var node = document.querySelector('em')
+    console.log('node.nodeName : '+ node.nodeName);
+    // node.nodeName : EM
+
+    // d3 selector
+    var d3node = d3.select('em');
+    console.log("Array.isArray(d3node) : " + Array.isArray(d3node));
+    // Array.isArray(d3node) : true 
+    // Array type으로 반환된다.
+
+    var el = d3node[0];
+    el[0].nodeName
+    console.log('el[0].nodeName : '+ el[0].nodeName);
+
+
 }
