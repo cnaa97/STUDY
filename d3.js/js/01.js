be877eae95cf2be369ec9f2229b48c794ddb536a
박준호 <junho@brunt.co>
2016-12-10 15:01:39 +0900

    d3 code

--- d3.js/js/01.js
+++ d3.js/js/01.js
@@ -1,4 +1,7 @@
 window.onload = function(){
+
+// 기본 SVG 도형 그리기
+
     d3.select(document.body)
     .append('svg')
     .attr('id','idsvg')
@@ -10,4 +13,10 @@ window.onload = function(){
         left:'100px',
         border:'1px solid blue'
     })
+    .append('rect') // 사각형 그리기
+    .attr({x:10, y:10, width:100, height:50, stroke:"blue", fill:"lime", "stroke-width":"5", rx:5, ry:5})
+    // d3.으로 시작하는 것은 select()로 시작되는 그룹을 만드는 것 wrapper method라고 생각하면 됨.
+    
+// select
+ 
 }
