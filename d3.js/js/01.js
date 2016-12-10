056a4322a0bf9320f3b787ab0951cb690798c616
박준호 <junho@brunt.co>
2016-12-10 14:31:58 +0900

    first d3 code

--- d3.js/js/01.js
+++ d3.js/js/01.js
@@ -1,3 +1,13 @@
 window.onload = function(){
-    
+    d3.select(document.body)
+    .append('svg')
+    .attr('id','idsvg')
+    .attr('width',150)
+    .attr('height',100)
+    .style({
+        position:'absolute',
+        top:'200px',
+        left:'100px',
+        border:'1px solid blue'
+    })
 }
