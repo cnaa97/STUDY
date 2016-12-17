b7aad3e2fef86c737715b60830a86c1e01344584
Junho Park <cnaa97@gmail.com>
2016-12-17 11:29:33 +0900

    add third d3

--- d3.js/js/03.js
+++ d3.js/js/03.js
@@ -1,7 +1,7 @@
 window.onload = function(){
 
 /** 그래프 그리기 */
-    
+
     // 데이터 정의하는 함수 설정
     function defineData(){
         window.base = {};
@@ -27,11 +27,11 @@ window.onload = function(){
     function createSVG(){
         return d3.select(document.body)
                 .append('svg')
-                .attr('id','idSvgGraph')     
+                .attr('id','idSvgGraph')
     }
 
     // (1)
-    // 사각형 박스 먼저 그리기 
+    // 사각형 박스 먼저 그리기
     var object = createSVG();
 
     function setBaseValue(object){
@@ -52,8 +52,138 @@ window.onload = function(){
         base.graphHeight = base.svgHeight - base.trbl.top - base.trbl.bottom;
 
     }
-    
-    setBaseValue(object);
-       
 
-}
+    setBaseValue(object);
+
+    // (2) Y축 눈금자
+    function drawYAxis(object){
+
+        // var yAxis = d3.svg.asix().scale(yScale).orient('left');
+        // d3.svg.axis()로 축 제어 인스턴스 생성
+        // scale()의 파라미터에 앞에서 작성한 yScale
+        base.yScale = d3.scale.linear()
+            //[0, 1000]은 데이터 범위
+            .domain([0, base.maxAmount])
+            // [base.graphHeight, 0]는 출력할 좌표 범위
+            // 그래프를 최상 꼭지점에서 우하로 그리므로
+            // graphHeight에 큰 값을 작성해야 1000, 9000 순서로 표시
+            .range([base.graphHeight, 0]);
+
+        // y축을 표시하기 위한 기준 설정
+        base.yAxis = d3.svg.axis()
+            .scale(base.yScale)
+            // 왼쪽에 표시
+            .orient('left');
+
+        // y축 표시
+        object.append('g')
+            //.attr('transform','translate('+ base.trbl.left + ','+ base.trbl.top + ')')
+            .attr('transform',`translate(${base.trbl.left},${base.trbl.top})`)
+            .append('g').attr('class','axis').call(base.yAxis);
+
+    }
+    drawYAxis(object);
+
+
+    // (3) x축 눈금자
+    function drawXAxis(object){
+
+        // 서수 형태로 배열 순서와 값 매핑
+        base.xScale = d3.scale.ordinal()
+            // 데이터에서 mon값을 범위로 지정한다
+            .domain(base.data.map(function(obj){
+                return obj.mon;
+            }))
+            // 소수가 아닌 정수 반환
+            .rangeRoundBands([0, base.graphWidth], .4);
+
+        base.xAxis = d3.svg.axis()
+            .scale(base.xScale)
+            // x축 아래에 표시
+            .orient('bottom');
+
+        object.append('g')
+            .attr('transform',`translate(${base.trbl.left},${base.svgHeight - base.trbl.bottom})`)
+            .attr('class','axis')
+            .call(base.xAxis);
+    }
+    drawXAxis(object);
+
+    var xAxisList = [];
+    // (4) 막대바 그리기
+    function drawBar(object){
+        object
+            .append('g')
+            .attr({
+                'class':'bar',
+                'transform' : `translate(${base.trbl.left},${base.trbl.top})`
+            })
+            .selectAll('rect')
+            .data(base.data)
+            .enter()
+            .append('rect')
+            .attr({
+                'x' : function(obj){
+                    var coord = base.xScale(obj.mon);
+                    xAxisList.push(coord);
+                    return base.xScale(obj.mon)
+                },
+                'width' : base.xScale.rangeBand(),
+                'y': function(obj){
+                    // 데이터가 950일 때 최대값이 1000이므로
+                    //  400 * (1 - 0.95) * 0.95 형태로 계산되며 결과는 20
+                    return base.yScale(obj.amt);
+                },
+                'height':function(obj){
+                    // 데이터가 950일때
+                    // 그래프 높이 400에서 20을 빼면 380
+                    return base.graphHeight - base.yScale(obj.amt);
+                }
+            });
+    }
+    drawBar(object);
+
+    function drawText(object){
+
+        object
+            .append('g')
+            .attr({
+                'transform' : `translate(${base.trbl.left},${base.trbl.top})`
+            })
+            .selectAll('g')
+            .data(base.data)
+            .enter()
+            .append('text')
+            .attr({
+                'class':'datatext',
+                'x' : function(obj, index){
+                    return xAxisList[index] + (base.xScale.rangeBand() / 2);
+                },
+                'y': base.graphHeight
+            })
+            .text(function(obj){
+                return obj.amt;
+            });
+    }
+    drawText(object);
+
+
+}// onload
+
+// coord = base.xScale(obj.mon)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
